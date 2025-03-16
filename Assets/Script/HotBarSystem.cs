8f74744b41a785529c1b7e7e309154a4f7d3e8c2
Đỗ Kiên <kien.dt224869@sis.hust.edu.vn>
2025-03-16 20:31:44 +0700

    update-selective-slot

--- Assets/Script/HotBarSystem.cs
+++ Assets/Script/HotBarSystem.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace Script
@@ -5,10 +6,15 @@ namespace Script
     public class HotBarSystem: MonoBehaviour
     {
         public GameObject hotBar;
-
+        public GameObject selectiveSlot;
         void Start()
         {
             PopulateSlot();
+        }
+
+        void Update()
+        {
+            GetSelectiveSlot();
         }
         void PopulateSlot()
         {
@@ -17,6 +23,38 @@ namespace Script
                 if (child.CompareTag("Slot"))
                 {
                     InventorySystem.Instance.slotList.Add(child.gameObject);
+                }
+            }
+        }
+
+        Vector3 GetPositionSlot(string slotName)
+        {
+            foreach (Transform child in hotBar.transform)
+            {
+                if (child.CompareTag("Slot"))
+                {
+                    if (child.name == slotName)
+                    {
+                        Debug.Log(child.localPosition);
+                        return child.localPosition;
+                    }
+                }
+            }
+            return Vector3.zero;
+        }
+
+        void GetSelectiveSlot()
+        {
+            for (int i = 1; i <= 9; i++)
+            {
+                if (Input.GetKeyDown((KeyCode)(KeyCode.Alpha0 + i))) 
+                {
+                    selectiveSlot.transform.localPosition = GetPositionSlot($"Slot {i}");
+                    break;
+                }
+                else if (Input.GetKeyDown((KeyCode)(KeyCode.Alpha0 )))
+                {
+                    selectiveSlot.transform.localPosition = GetPositionSlot("Slot 10");
                 }
             }
         }
